49700b50d593763a3da825828a11b22cb7ba4ee2
artem2284708 <artemmmmkk@gmail.com>
2025-03-28 18:42:54 +0300

    New function (news_parse) added

--- parsing/src/parsers/main.py
+++ parsing/src/parsers/main.py
@@ -2,6 +2,7 @@ from club_salary_parse import club_salary_parse
 from player_salary_parse import player_salary_parse
 from season_summary import season_summary
 from player_advanced import player_advanced
+from news_parse import news_parse
 
 
 # comment which is NOT needed to be parsed
@@ -9,7 +10,9 @@ def main():
     # club_salary_parse()
     # player_salary_parse()
     # season_summary()
-    player_advanced()
+    # player_advanced()
+    news_parse()
+
 
 if __name__ == '__main__':
     main()
